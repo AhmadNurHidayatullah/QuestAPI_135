d0744ec1e31fb139b49cb8a3b30572fd8f4a88e0
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 16:09:38 +0700

    Tambahkan object DestinasiEntry untuk navigasi.

---
+++ app/src/main/java/com/example/consumerestapi/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.consumerestapi.ui.view
+
+import com.example.consumerestapi.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
