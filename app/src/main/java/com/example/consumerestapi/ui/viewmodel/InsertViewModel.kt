9515a44ec0c9cede76dad7761a63b63f3f9dce4f
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 15:21:45 +0700

    Menambahkan fungsi untuk mengubah data Mahasiswa menjadi InsertUiState.

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,10 @@ package com.example.consumerestapi.ui.viewmodel
 
 import com.example.consumerestapi.model.Mahasiswa
 
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
 fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     nim = nim,
     nama = nama,
