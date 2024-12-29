8ef83fd2f6f65b67b557154226126885cd4e1571
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 15:24:37 +0700

    Menambahkan fungsi untuk mengubah data InsertUiEvent menjadi objek Mahasiswa.

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,15 @@ package com.example.consumerestapi.ui.viewmodel
 
 import com.example.consumerestapi.model.Mahasiswa
 
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
