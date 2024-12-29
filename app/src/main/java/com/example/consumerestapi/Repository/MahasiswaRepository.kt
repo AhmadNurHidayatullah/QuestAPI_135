54773437f1b87760872f931b39151d9521df1404
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 14:54:49 +0700

    Upload tugas yang sudah dikerjakan dikelas

---
+++ app/src/main/java/com/example/consumerestapi/Repository/MahasiswaRepository.kt
@@ -0,0 +1,43 @@
+package com.example.consumerestapi.Repository
+
+import com.example.consumerestapi.model.Mahasiswa
+import com.example.consumerestapi.service.MahasiswaService
+import okio.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String,mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val MahasiswaAPIService: MahasiswaService
+): MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        MahasiswaAPIService.insertMahasiswa(mahasiswa)
+    }
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        MahasiswaAPIService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = MahasiswaAPIService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("failed to delete mahasiswa. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = MahasiswaAPIService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return MahasiswaAPIService.getMahasiswaById(nim)
+    }
+}
