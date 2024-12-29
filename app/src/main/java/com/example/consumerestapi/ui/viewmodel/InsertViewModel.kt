580371a8d7c8dca64a71dfa4c4bee48cd29611bf
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 15:18:48 +0700

    Menambahkan fungsi konversi dari model Mahasiswa ke InsertUiEvent.

---
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.consumerestapi.ui.viewmodel
+
+import com.example.consumerestapi.model.Mahasiswa
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
