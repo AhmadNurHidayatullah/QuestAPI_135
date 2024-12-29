6e2eb2f77f9bec38b81c4699e6b5f6f8e7850135
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-29 15:28:03 +0700

    Menambahkan data class InsertUiEvent untuk menyimpan informasi mahasiswa.

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,15 @@ package com.example.consumerestapi.ui.viewmodel
 
 import com.example.consumerestapi.model.Mahasiswa
 
+data class InsertUiEvent(
+    val nim:String="",
+    val nama:String="",
+    val alamat:String="",
+    val jenisKelamin:String="",
+    val kelas:String="",
+    val angkatan:String=""
+)
+
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
