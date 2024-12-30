1cdf9289ba738999bcd3c934438213bb84691d6d
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:21:18 +0700

    Menambahkan UpdateBody

--- app/src/main/java/com/example/consumerestapi/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/consumerestapi/ui/view/UpdateView.kt
@@ -1,22 +1,32 @@
 package com.example.consumerestapi.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.ui.customwidget.CostumeTopAppBar
 import com.example.consumerestapi.ui.navigation.DestinasiNavigasi
 import com.example.consumerestapi.ui.viewmodel.PenyediaViewModel
+import com.example.consumerestapi.ui.viewmodel.UpdateUiEvent
+import com.example.consumerestapi.ui.viewmodel.UpdateUiState
 import com.example.consumerestapi.ui.viewmodel.UpdateViewModel
 import kotlinx.coroutines.launch
 
@@ -66,5 +76,39 @@ fun UpdateScreen(
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onSiswaValueChange: (UpdateUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.extraLarge,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text(
+                    text = "Update",
+                    fontSize = 16.sp
+                )
+            }
+        }
     }
 }
