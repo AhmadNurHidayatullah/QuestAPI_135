d8a0269471631b41dc75a247c41a94978821d872
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:07:05 +0700

    Menambahkan fungsi toMhs

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
@@ -2,6 +2,15 @@ package com.example.consumerestapi.ui.viewmodel
 
 import com.example.consumerestapi.model.Mahasiswa
 
+fun UpdateUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUpdateUiStateMhs(): UpdateUiState = UpdateUiState(
     updateUiEvent = toUpdateUiEvent()
 )
