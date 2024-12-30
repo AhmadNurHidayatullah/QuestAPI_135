6bdb00f9b6c81bd55bf8f22de3d53d64f5c05319
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:12:44 +0700

    Menambahkan penanganan state pada UpdateViewModel.

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
@@ -1,6 +1,68 @@
 package com.example.consumerestapi.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumerestapi.Repository.MahasiswaRepository
 import com.example.consumerestapi.model.Mahasiswa
+import kotlinx.coroutines.launch
+
+sealed class UpdtUIState{
+    data class  Success(val mahasiswa: List<Mahasiswa>): UpdtUIState()
+    object Error : UpdtUIState()
+    object Loading : UpdtUIState()
+}
+
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+
+    var updateUiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun getMahasiswaById(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                updateUiState = updateUiState.copy(
+                    updateUiEvent = UpdateUiEvent(
+                        nim = mahasiswa.nim,
+                        nama = mahasiswa.nama,
+                        alamat = mahasiswa.alamat,
+                        jenisKelamin = mahasiswa.jenisKelamin,
+                        kelas = mahasiswa.kelas,
+                        angkatan = mahasiswa.angkatan
+                    )
+                )
+            } catch (e: Exception) {
+                UpdtUIState.Error
+            }
+        }
+    }
+
+    fun updateMhsState(updateUiEvent: UpdateUiEvent) {
+        updateUiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    fun loadMahasiswa(mahasiswa: Mahasiswa) {
+        updateUiState = mahasiswa.toUpdateUiStateMhs()
+    }
+
+    fun updateMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(
+                    nim = updateUiState.updateUiEvent.nim,
+                    mahasiswa = updateUiState.updateUiEvent.toMhs()
+                )
+            } catch (e: Exception) {
+                UpdtUIState.Error
+            }
+        }
+    }
+}
+
+
 
 data class UpdateUiState(
     val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
