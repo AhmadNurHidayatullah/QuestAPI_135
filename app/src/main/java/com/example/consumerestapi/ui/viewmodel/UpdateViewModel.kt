64c95a942ea69383eedd94421031538e12d7b120
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:07:45 +0700

    Menambahkan data class UpdateUiEvent

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/UpdateViewModel.kt
@@ -2,6 +2,15 @@ package com.example.consumerestapi.ui.viewmodel
 
 import com.example.consumerestapi.model.Mahasiswa
 
+data class UpdateUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun UpdateUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
