88f318404a45422f1390d66da085254fa150e0c6
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:26:08 +0700

    Menambahkan FormInput UI

--- app/src/main/java/com/example/consumerestapi/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/consumerestapi/ui/view/UpdateView.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,6 +22,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -112,3 +116,84 @@ fun UpdateBody(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        OutlinedTextField(
+            value = updateUiEvent.nama,
+            onValueChange = { onValueChange(updateUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            placeholder = { Text("Masukkan Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.nim,
+            onValueChange = { onValueChange(updateUiEvent.copy(nim = it)) },
+            label = { Text("Nim") },
+            placeholder = { Text("Masukkan Nim") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = true,
+            singleLine = true
+        )
+        Text("Jenis Kelamin", style = MaterialTheme.typography.bodyMedium)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(
+                value = updateUiEvent.jenisKelamin,
+                onValueChange = { onValueChange(updateUiEvent.copy(jenisKelamin = it)) },
+                label = { Text("Jenis Kelamin") },
+                placeholder = { Text("Masukkan Jenis Kelamin") },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = enabled,
+                singleLine = true
+            )
+        }
+        OutlinedTextField(
+            value = updateUiEvent.alamat,
+            onValueChange = { onValueChange(updateUiEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            placeholder = { Text("Masukkan Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        Text("Kelas", style = MaterialTheme.typography.bodyMedium)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(
+                value = updateUiEvent.kelas,
+                onValueChange = { onValueChange(updateUiEvent.copy(kelas = it)) },
+                label = { Text("Kelas") },
+                placeholder = { Text("Masukkan Kelas") },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = enabled,
+                singleLine = true
+            )
+        }
+        OutlinedTextField(
+            value = updateUiEvent.angkatan,
+            onValueChange = { onValueChange(updateUiEvent.copy(angkatan = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Angkatan") },
+            placeholder = { Text("Masukkan Angkatan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        Divider(
+            thickness = 5.dp,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
