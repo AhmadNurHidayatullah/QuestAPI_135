4f5b46c104432e9ab550a63b2a37839ce23ebb92
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 13:15:30 +0700

    Menambahkan objek DestinasiUpdate

---
+++ app/src/main/java/com/example/consumerestapi/ui/view/UpdateView.kt
@@ -0,0 +1,8 @@
+package com.example.consumerestapi.ui.view
+
+import com.example.consumerestapi.ui.navigation.DestinasiNavigasi
+
+object DestinasiUpdate : DestinasiNavigasi {
+    override val route = "update_view"
+    override val titleRes = "Update Mahasiswa"
+}
