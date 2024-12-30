12dddb7d75e59aabc4bbdd75b7d66852bbce6ee3
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-30 10:36:56 +0700

    Menambahkan fungsi CreationExtras.aplikasiKontak.

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/PenyediaViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.consumerestapi.ui.viewmodel
 
 import android.text.Editable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.MahasiswaAplications
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -10,3 +13,6 @@ object PenyediaViewModel {
         initializer { InsertViewModel(aplikasiKontak().container.KontakRepository) }
     }
 }
+
+fun CreationExtras.aplikasiKontak():MahasiswaAplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaAplications)
